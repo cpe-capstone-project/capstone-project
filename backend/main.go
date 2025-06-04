1efbeb61ed79745cb39e06f3807899a816e01814
tyliw <tiw.ravipon1234@gmail.com>
2025-06-04 15:02:09 +0700

    ตั้งค่าข้อมูลตัวอย่างของระบบบันทึกไดอารี่ - close #12

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,13 @@
 package main
 
+import "capstone-project/config"
+
 func main() {
+	// เปิดการเชื่อมต่อฐานข้อมูล
+	config.ConnectionDB()
+
+	// สร้างฐานข้อมูล
+	config.SetupDatabase()
+	
 	// r.Run("localhost:" + routers.PORT)
 }

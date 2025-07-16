7a40afcb6808a6dd13c97d0cad142fa99a556b1f
Thkawaii <xxtheeparatxx@gmail.com>
2025-07-16 17:53:23 +0700

    api update psychologist

--- backend/routers/psychologist.go
+++ backend/routers/psychologist.go
@@ -1,9 +1,9 @@
 package routers
 
 import (
-
 	psyauth "capstone-project/controller/psychology"
-	
+	"capstone-project/middlewares"
+
 	psyuser "capstone-project/controller/psychology"
 
 	"github.com/gin-gonic/gin"
@@ -14,6 +14,8 @@ func SetupPsychologistRoutes(r *gin.Engine) {
 	r.POST("/psychologists/register", psyauth.Register)
 	r.POST("/psychologists/login", psyauth.Login)
 	r.POST("/psychologist/reset-password", psyauth.ResetPassword)
+	r.GET("/psychologist/profile", middlewares.Authorizes(), psyuser.GetProfile)
+	r.PUT("/psychologist/update-profile", middlewares.Authorizes(), psyuser.UpdateProfile)
 	// User routes
 	r.GET("/psychologists", psyuser.ListPsychologists)
 	r.GET("/psychologists/:id", psyuser.GetPsychologistByID)

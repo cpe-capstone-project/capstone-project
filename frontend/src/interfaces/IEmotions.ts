557fba04a93a120e21e76cecebd1711fc5bfe4c7
Pm1339900770914 <nutthaphum25@gmail.com>
2025-09-03 13:46:57 +0700

    create emotion services and interfaces

---
+++ frontend/src/interfaces/IEmotions.ts
@@ -0,0 +1,10 @@
+export interface EmotionsInterface {
+  ID?: number;
+  CreatedAt?: string; 
+  UpdatedAt?: string;
+  DeletedAt?: string | null;
+  Emotionsname?: string;
+  Category?: string;
+  ThaiEmotionsname?: string;
+  EmotionsColor?: string;
+}
